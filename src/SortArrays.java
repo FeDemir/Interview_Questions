b1364248c0c607a88a02a65e8e13d977f621507b
FeDemir <dmrfrht@gmail.com>
2021-10-23 13:48:00 -0500

    Q11: Array -- Sort Ascending Write a return method that can sort an int array in Ascending order without using the sort method of the Arrays class Q12: Array -- Sort Descending Write a return method that can sort an int array in descending order without using the sort method of the Arrays class

--- src/SortArrays.java
+++ src/SortArrays.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class SortAscending {
+public class SortArrays {
     /*
     Q11: Array -- Sort Ascending
     Write a return method that can sort an int array in Ascending order without using the sort method of the Arrays class
